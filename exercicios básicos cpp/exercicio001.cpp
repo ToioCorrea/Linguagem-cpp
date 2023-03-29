35dccd4c0d497acbd532651d6e2e4d85876c49b8
Bruno Amorim <119983682+ToioCorrea@users.noreply.github.com>
2023-03-29 17:15:15 -0300

    exercicio 001

---
+++ exercicios básicos cpp/exercicio001.cpp
@@ -0,0 +1,20 @@
+/*
+Escreva um programa que contenha uma unica instrução e imprima na tela: 
+
+um
+    dois
+         tres
+
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "um \n" << '\t' << "dois \n" << '\t' << '\t' << "tres" ;
+
+
+}
